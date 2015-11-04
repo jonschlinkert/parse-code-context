85d631522ccaa8314075ad5fed7f36b744e5bce9
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-04 07:55:29 -0500

    refactor

--- index.js
+++ index.js
@@ -1,112 +1,319 @@
-/*!
- * parse-code-context <https://github.com/jonschlinkert/parse-code-context>
- * Regex originally sourced and modified from <https://github.com/visionmedia/dox>.
- *
- * Copyright (c) 2014-2015 Jon Schlinkert.
- * Licensed under the MIT license.
- */
-
 'use strict';
 
-module.exports = function (str, i) {
-  var match = null;
+/**
+ * Create an instance of `Parser` with
+ * the given `string`, optionally passing
+ * a `parent` name for namespacing methods
+ *
+ * ```js
+ * var parser = new Parser('function foo(a, b, c){}');
+ * ```
+ * @param {String} `str`
+ * @param {String} `parent`
+ * @api public
+ */
 
-  // function statement
-  if (match = /^function[ \t]([\w$]+)[ \t]*([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'function statement',
-      name: match[1],
-      params: (match[2]).split(/\W+/).filter(Boolean),
-      string: match[1] + '()',
-      original: str
-    };
-    // function expression
-  } else if (match = /^var[ \t]*([\w$]+)[ \t]*=[ \t]*function([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'function expression',
-      name: match[1],
-      params: (match[2]).split(/\W+/).filter(Boolean),
-      string: match[1] + '()',
-      original: str
-    };
-    // module.exports expression
-  } else if (match = /^(module\.exports)[ \t]*=[ \t]*function[ \t]([\w$]+)[ \t]*([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'function expression',
-      receiver: match[1],
-      name: match[2],
-      params: (match[3]).split(/\W+/).filter(Boolean),
-      string: match[1] + '()',
-      original: str
-    };
-    // module.exports method
-  } else if (match = /^(module\.exports)[ \t]*=[ \t]*function([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'method',
-      receiver: match[1],
-      name: '',
-      params: (match[2]).split(/\W+/).filter(Boolean),
-      string: match[1] + '.' + match[2] + '()',
-      original: str
-    };
-    // prototype method
-  } else if (match = /^([\w$]+)\.prototype\.([\w$]+)[ \t]*=[ \t]*function([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'prototype method',
-      class: match[1],
-      name: match[2],
-      params: (match[3]).split(/\W+/).filter(Boolean),
-      string: match[1] + '.prototype.' + match[2] + '()',
-      original: str
-    };
-    // prototype property
-  } else if (match = /^([\w$]+)\.prototype\.([\w$]+)[ \t]*=[ \t]*([^\n;]+)/.exec(str)) {
-    return {
-      start: i,
-      type: 'prototype property',
-      class: match[1],
-      name: match[2],
-      value: match[3],
-      string: match[1] + '.prototype.' + match[2],
-      original: str
-    };
-    // method
-  } else if (match = /^([\w$.]+)\.([\w$]+)[ \t]*=[ \t]*function([\w\W]+)?/.exec(str)) {
-    return {
-      start: i,
-      type: 'method',
-      receiver: match[1],
-      name: match[2],
-      params: (match[3]).split(/\W+/).filter(Boolean),
-      string: match[1] + '.' + match[2] + '()',
-      original: str
-    };
-    // property
-  } else if (match = /^([\w$]+)\.([\w$]+)[ \t]*=[ \t]*([^\n;]+)/.exec(str)) {
-    return {
-      start: i,
-      type: 'property',
-      receiver: match[1],
-      name: match[2],
-      value: match[3],
-      string: match[1] + '.' + match[2],
-      original: str
-    };
-    // declaration
-  } else if (match = /^var[ \t]+([\w$]+)[ \t]*=[ \t]*([^\n;]+)/.exec(str)) {
-    return {
-      start: i,
-      type: 'declaration',
-      name: match[1],
-      value: match[2],
-      string: match[1],
-      original: str
-    };
+function Parser(str, parent) {
+  this.string = str;
+  this.parent = parent;
+  this.fns = [];
+}
+
+/**
+ * Convenience method for creating a property name
+ * that is prefixed with the parent namespace, if defined.
+ *
+ * @param {String} `name`
+ * @return {String}
+ * @api public
+ */
+
+Parser.prototype.name = function(name) {
+  return this.parent ? (this.parent + name) : '';
+};
+
+/**
+ * Register a parser to use (in addition to those already
+ * registered as default parsers) with the given `regex` and
+ * function.
+ *
+ * ```js
+ * var parser = new Parser('function foo(a, b, c){}');
+ *   .use(/function\s*([\w$]+)\s*\(([^)]+)/, function(match) {
+ *     return {
+ *        name: match[1],
+ *        params: match[2].split(/[, ]/)
+ *     };
+ *   })
+ * ```
+ * @param {RegExp} `regex`
+ * @param {Function} `fn`
+ * @return {Object} The instance for chaining
+ * @api public
+ */
+
+Parser.prototype.use = function(regex, fn) {
+  this.fns.push({regex: regex, fn: fn});
+  return this;
+};
+
+/**
+ * Parse the string passed to the constructor with
+ * all registered parsers.
+ *
+ * @return {Object|Null}
+ * @api public
+ */
+
+Parser.prototype.parse = function() {
+  this.init();
+  var len = this.fns.length;
+  var i = -1;
+
+  while (++i < len) {
+    var parser = this.fns[i];
+    var re = parser.regex;
+    var fn = parser.fn;
+    var match = re.exec(this.string);
+    if (match) {
+      var ctx = fn.call(this, match, this.parent);
+      if (ctx) {
+        this.value = ctx;
+        return ctx;
+      }
+    }
   }
   return null;
 };
+
+Parser.prototype.init = function() {
+  // module.exports method
+  this.use(/^(module\.exports)\s*=\s*function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'method',
+      receiver: m[1],
+      name: '',
+      params: (m[2]).split(/[, ]+/),
+      string: m[1] + '.' + m[2] + '()',
+      original: m.input
+    };
+  });
+
+  this.use(/^(module\.exports)\s*=\s*function\s([\w$]+)\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'function',
+      subtype: 'expression',
+      receiver: m[1],
+      name: m[2],
+      params: (m[3]).split(/[, ]+/),
+      string: m[2] + '()',
+      original: m.input
+    };
+  });
+  // class, possibly exported by name or as a default
+  this.use(/^\s*(export(\s+default)?\s+)?class\s+([\w$]+)(\s+extends\s+([\w$.]+(?:\(.*\))?))?\s*{/, function(m) {
+    return {
+      type: 'class',
+      ctor: m[3],
+      name: m[3],
+      extends: m[5],
+      string: 'new ' + m[3] + '()'
+    };
+  });
+
+  // class constructor
+  this.use(/^\s*constructor\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'constructor',
+      ctor: this.parent,
+      name: 'constructor',
+      params: (m[4] || '').split(/[, ]+/),
+      string: this.name('.prototype.') + 'constructor()'
+    };
+  });
+
+  // class method
+  this.use(/^\s*(static)?\s*(\*)?\s*([\w$]+|\[.*\])\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'method',
+      ctor: this.parent,
+      name: m[2] + m[3],
+      params: m[4].split(/[, ]+/),
+      string: this.name(m[1] ? '.' : '.prototype.') + m[2] + m[3] + '()'
+    };
+  });
+
+  // named function statement, possibly exported by name or as a default
+  this.use(/^\s*(export(\s+default)?\s+)?function\s+([\w$]+)\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'function',
+      subtype: 'statement',
+      name: m[3],
+      params: m[4].split(/[, ]+/),
+      string: m[3] + '()'
+    };
+  });
+
+  // anonymous function expression exported as a default
+  this.use(/^\s*export\s+default\s+function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'function',
+      name: m[1], // undefined
+      params: m[4].split(/[, ]+/),
+      string: m[1] + '()'
+    };
+  });
+
+  // function expression
+  this.use(/^return\s+function(?:\s+([\w$]+))?\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'function',
+      subtype: 'expression',
+      name: m[1],
+      params: m[4].split(/[, ]+/),
+      string: m[1] + '()'
+    };
+  });
+
+  // function expression
+  this.use(/^\s*(?:const|let|var)\s+([\w$]+)\s*=\s*function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'function',
+      subtype: 'expression',
+      name: m[1],
+      params: m[2].split(/[, ]+/),
+      string: (m[1] || '') + '()'
+    };
+  });
+
+  // prototype method
+  this.use(/^\s*([\w$.]+)\s*\.\s*prototype\s*\.\s*([\w$]+)\s*=\s*function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'prototype method',
+      category: 'method',
+      ctor: m[1],
+      name: m[2],
+      params: m[3].split(/[, ]+/),
+      string: m[1] + '.prototype.' + m[2] + '()'
+    };
+  });
+
+  // prototype property
+  this.use(/^\s*([\w$.]+)\s*\.\s*prototype\s*\.\s*([\w$]+)\s*=\s*([^\n;]+)/, function(m) {
+    return {
+      type: 'prototype property',
+      ctor: m[1],
+      name: m[2],
+      value: m[3].trim(),
+      string: m[1] + '.prototype.' + m[2]
+    };
+  });
+
+  // prototype property without assignment
+  this.use(/^\s*([\w$]+)\s*\.\s*prototype\s*\.\s*([\w$]+)\s*/, function(m) {
+    return {
+      type: 'prototype property',
+      ctor: m[1],
+      name: m[2],
+      string: m[1] + '.prototype.' + m[2]
+    };
+  });
+
+  // inline prototype
+  this.use(/^\s*([\w$.]+)\s*\.\s*prototype\s*=\s*{/, function(m) {
+    return {
+      type: 'prototype',
+      ctor: m[1],
+      name: m[1],
+      string: m[1] + '.prototype'
+    };
+  });
+
+  // Fat arrow function
+  this.use(/^\s*\(*\s*([\w$.]+)\s*\)*\s*=>/, function(m) {
+    return {
+      type: 'function',
+      ctor: this.parent,
+      name: m[1],
+      string: this.name('.prototype.') + m[1] + '()'
+    };
+  });
+
+  // inline method
+  this.use(/^\s*([\w$.]+)\s*:\s*function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'method',
+      ctor: this.parent,
+      name: m[1],
+      string: this.name('.prototype.') + m[1] + '()'
+    };
+  });
+
+  // inline property
+  this.use(/^\s*([\w$.]+)\s*:\s*([^\n;]+)/, function(m) {
+    return {
+      type: 'property',
+      ctor: this.parent,
+      name: m[1],
+      value: m[2].trim(),
+      string: this.name('.') + m[1]
+    };
+  });
+
+  // inline getter/setter
+  this.use(/^\s*(get|set)\s*([\w$.]+)\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'property',
+      ctor: this.parent,
+      name: m[2],
+      string: this.name('.prototype.') + m[2]
+    };
+  });
+
+  // method
+  this.use(/^\s*([\w$.]+)\s*\.\s*([\w$]+)\s*=\s*function\s*\(([^)]+)/, function(m) {
+    return {
+      type: 'method',
+      receiver: m[1],
+      name: m[2],
+      params: m[3].split(/[, ]+/),
+      string: m[1] + '.' + m[2] + '()'
+    };
+  });
+
+  // property
+  this.use(/^\s*([\w$.]+)\s*\.\s*([\w$]+)\s*=\s*([^\n;]+)/, function(m) {
+    return {
+      type: 'property',
+      receiver: m[1],
+      name: m[2],
+      value: m[3].trim(),
+      string: m[1] + '.' + m[2]
+    };
+  });
+
+  // declaration
+  this.use(/^\s*(?:const|let|var)\s+([\w$]+)\s*=\s*([^\n;]+)/, function(m) {
+    return {
+      type: 'declaration',
+      name: m[1],
+      value: m[2].trim(),
+      string: m[1]
+    };
+  });
+};
+
+/**
+ * Expose an instance of `Parser`
+ */
+
+module.exports = function(str, ctx, i) {
+  var parser = new Parser(str, ctx, i);
+  return parser.parse();
+};
+
+/**
+ * Expose `Parser`
+ */
+
+module.exports.Parser = Parser;
